99d07047597366fd4cfb066f5c03e78d1bbc0c11
SansPseudoFix <spf@sanspseudofix.fr>
2014-08-21 16:45:46 +0200

    Fichier 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php theme_include('header'); ?>
+
+	<section class="content wrap">
+		<h1>Oups...</h1>
+
+		<p>Malheureusement, la page /<?php echo current_url(); ?> n'a pas pu être trouvé. Revenez plutôt à la <a href="<?php echo base_url(); ?>">page d'accueil</a>.</p>
+	</section>
+
+<?php theme_include('footer'); ?>
